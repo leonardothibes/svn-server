fd33eff7c8252974e5bafcdfa0d3a5495d27c356
Leonardo Thibes <eu@leonardothibes.com>
2014-06-25 14:47:39 -0300

    implementando instalacao do s3cmd

--- puppet/modules/backup/manifests/init.pp
+++ puppet/modules/backup/manifests/init.pp
@@ -6,6 +6,11 @@ class backup
 		mode  => 0755,
 	}
 
+	file {'/home/root':
+		ensure => link,
+		target => '/root',
+	}
+
 	$bkpdir = '/var/lib/backup-scripts'
 	file {'backup::directory':
 		ensure => directory,
